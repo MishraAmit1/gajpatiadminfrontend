238486a472f8c798f7efcbe4b790df9fa7f9ba01
MishraAmit1 <amsmishra1510@gmail.com>
2025-07-28 18:27:03 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-    proxy: {
-      "/api":
-        "https://305ycfaNP82pEMpZMf2nyvBvcXm_j6kTQS3BUbdBvFj4Xfwq.ngrok-free.app",
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
