2e482e531210fd6fe2eb23dd8e8f50d1177e6d57
MishraAmit1 <amsmishra1510@gmail.com>
2025-07-31 15:43:40 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://localhost:3000/api/v1";
-const API_BASE_URL = "https://gajpati-backend.onrender.com";
+const API_BASE_URL = "https://gajpati-backend.onrender.com/api/v1";
 
 class ApiService {
   constructor() {
